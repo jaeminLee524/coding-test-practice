40fcb76d3409a75b8a91fa136aa1f965cacac0f6
이재민 <rkwoals524@hanmail.net>
2024-07-14 14:25:19 +0900

    [배열]프로그래머스-42840

---
+++ src/main/java/com/study/book/array/ArrayExample4.java
@@ -0,0 +1,38 @@
+package com.study.book.array;
+
+import java.util.Arrays;
+
+public class ArrayExample4 {
+
+    private static int[][] solution(int[][] arr1, int[][] arr2) {
+
+        int r1 = arr1.length;
+        int c1 = arr1[0].length;
+
+        int r2 = arr2.length;
+        int c2 = arr2[0].length;
+
+        // 정답 배열
+        int[][] answers = new int[r1][c2];
+
+        for (int i = 0; i < r1; i++) {
+            for (int j = 0; j < c2; j++) {
+                for (int k = 0; k < c1; k++) {
+                    answers[i][j] += arr1[i][k] * arr2[k][j];
+                }
+            }
+        }
+
+        return answers;
+    }
+
+    public static void main(String[] args) {
+
+        int[][] arr1 = new int[][]{{1, 4}, {3, 2}, {4, 1}};
+        int[][] arr2 = new int[][]{{3, 3}, {3, 3}};
+
+        int[][] output1 = new int[][]{{15, 15}, {15, 15}, {15, 15}};
+
+        System.out.println(Arrays.equals(output1, ArrayExample4.solution(arr1, arr2)));
+    }
+}
