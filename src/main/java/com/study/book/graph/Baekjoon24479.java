6e7dc553e404b384702bfd7792080beeb409ff3b
이재민 <rkwoals524@hanmail.net>
2024-08-19 16:46:36 +0900

    [그래프] 백준-24479

---
+++ src/main/java/com/study/book/graph/Baekjoon24479.java
@@ -0,0 +1,68 @@
+package com.study.book.graph;
+
+import java.util.*;
+import java.io.*;
+
+public class Baekjoon24479 {
+
+    private static ArrayList<Integer>[] adjList;
+    private static boolean[] visited;
+    private static int[] answer;
+    private static int visitOrder;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int R = Integer.parseInt(st.nextToken());
+
+        adjList = new ArrayList[N + 1];
+        for (int i = 0; i < adjList.length; i++) {
+            adjList[i] = new ArrayList<>();
+        }
+
+        for (int i = 1; i < N + 1; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            adjList[x].add(y);
+            adjList[y].add(x);
+        }
+
+        for (ArrayList<Integer> list : adjList) {
+            Collections.sort(list);
+        }
+
+        visited = new boolean[N + 1];
+        answer = new int[N + 1];
+        visitOrder = R;
+
+        dfs(R);
+
+        for (int i = 1; i <= N; i++) {
+            bw.write(String.valueOf(answer[i]));
+            bw.newLine();
+        }
+
+        br.close();
+        bw.close();
+    }
+
+    private static void dfs(int now) {
+        visited[now] = true;
+        answer[now] = visitOrder;
+        visitOrder++;
+
+        for (int next : adjList[now]) {
+            if (!visited[next]) {
+                dfs(next);
+            }
+        }
+    }
+}
